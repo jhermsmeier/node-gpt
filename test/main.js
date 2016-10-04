16a805d43d616f9b4a9113d1d97c02c50677df53
Jonas Hermsmeier <jhermsmeier@gmail.com>
2016-10-04 19:01:38 +0200

    Update test: Enable trim whitespace

--- test/main.js
+++ test/main.js
@@ -3,18 +3,18 @@ var assert = require( 'assert' )
 var GPT = require( '../' )
 
 describe( 'GUID Partition Table', function() {
-  
+
   describe( 'BOOTCAMP', function() {
-    
+
     const DATAPATH = __dirname + '/data/bootcamp.bin'
     var data = fs.readFileSync( DATAPATH )
     var gpt = null
-    
+
     it( 'should be able to parse a BootCamp GPT', function() {
       gpt = GPT.parse( data )
       // console.log( gpt )
     })
-    
+
     it( 'in/out buffer equality', function() {
       var buffer = gpt.toBuffer()
       // fs.writeFileSync( __dirname + '/data/bootcamp.diff.bin', buffer )
@@ -23,20 +23,20 @@ describe( 'GUID Partition Table', function() {
       assert.equal( data.length, buffer.length )
       assert.deepEqual( data, buffer )
     })
-    
+
   })
-  
+
   describe( 'RASPBERRY PI MBR', function() {
-    
+
     const DATAPATH = __dirname + '/data/raspberry.bin'
     var data = fs.readFileSync( DATAPATH )
-    
+
     it( 'should throw "Invalid Signature"', function() {
       assert.throws( function() {
         GPT.parse( data )
       })
     })
-    
+
   })
-  
+
 })
