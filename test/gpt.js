357533a99d24fe01da33f9858e348c4c56581af4
Jonas Hermsmeier <jhermsmeier@gmail.com>
2024-02-22 23:46:47 +0100

    test: Replace `mocha` with `control`

--- test/gpt.js
+++ test/gpt.js
@@ -5,69 +5,69 @@ var MBR = require( 'mbr' )
 var GPT = require( '../' )
 var inspect = require( './inspect' )
 
-describe( 'GUID Partition Table', function() {
+context( 'GUID Partition Table', function() {
 
   // TODO:
   // - Add a test GPT blob with entries set to zero
   //   (the thing that still crashes Windows due to division by zero)
   // - Add tests with different blocksize disks (512, 2048, 4096, 8192)
 
-  describe( 'BOOTCAMP', function() {
+  context( 'BOOTCAMP', function() {
 
     var data = fs.readFileSync( path.join( __dirname, 'data', 'bootcamp.bin' ) )
     var backupData = fs.readFileSync( path.join( __dirname, 'data', 'bootcamp-backup.bin' ) )
 
-    it( 'should be able to parse a BootCamp GPT', function() {
+    test( 'should be able to parse a BootCamp GPT', function() {
       var gpt = GPT.parse( data )
       assert.equal( gpt.tableSize, 32 * gpt.blockSize )
       inspect.log( gpt )
     })
 
-    it( 'in/out buffer equality', function() {
+    test( 'in/out buffer equality', function() {
       var gpt = GPT.parse( data )
       var buffer = gpt.write()
       assert.equal( data.length, buffer.length )
       assert.deepEqual( data, buffer )
     })
 
-    it( 'verifies', function() {
+    test( 'verifies', function() {
       var gpt = GPT.parse( data )
       assert.ok( gpt.verify() )
     })
 
-    it( 'should be able to parse a backup GPT', function() {
+    test( 'should be able to parse a backup GPT', function() {
       var gpt = GPT.parseBackup( backupData )
       inspect.log( gpt )
     })
 
-    it( 'backup in/out buffer equality', function() {
+    test( 'backup in/out buffer equality', function() {
       var gpt = GPT.parseBackup( backupData )
       var buffer = gpt.writeBackup()
       assert.equal( backupData.length, buffer.length )
       assert.deepEqual( backupData, buffer )
     })
 
-    it( 'verifies a backup GPT', function() {
+    test( 'verifies a backup GPT', function() {
       var gpt = GPT.parseBackup( backupData )
       assert.ok( gpt.verify() )
     })
 
   })
 
-  describe( 'ExFAT', function() {
+  context( 'ExFAT', function() {
 
     var blockSize = 512
     var data = fs.readFileSync( path.join( __dirname, 'data', 'gpt-primary-exfat.bin' ) )
     var backupData = fs.readFileSync( path.join( __dirname, 'data', 'gpt-backup-exfat.bin' ) )
 
-    it( 'should be able to parse a BootCamp GPT', function() {
+    test( 'should be able to parse a BootCamp GPT', function() {
       var mbr = MBR.parse( data )
       var gpt = GPT.parse( data, mbr.getEFIPart().firstLBA * blockSize )
       assert.equal( gpt.tableSize, 32 * gpt.blockSize )
       inspect.log( gpt )
     })
 
-    it( 'in/out buffer equality', function() {
+    test( 'in/out buffer equality', function() {
       var mbr = MBR.parse( data )
       var offset = mbr.getEFIPart().firstLBA * blockSize
       var gpt = GPT.parse( data, offset )
@@ -76,31 +76,31 @@ describe( 'GUID Partition Table', function() {
       assert.deepEqual( data.slice( offset ), buffer )
     })
 
-    it( 'verifies', function() {
+    test( 'verifies', function() {
       var mbr = MBR.parse( data )
       var offset = mbr.getEFIPart().firstLBA * blockSize
       var gpt = GPT.parse( data, offset )
       assert.ok( gpt.verify() )
     })
 
-    it( 'should be able to parse a backup GPT', function() {
+    test( 'should be able to parse a backup GPT', function() {
       var gpt = GPT.parseBackup( backupData )
       inspect.log( gpt )
     })
 
-    it( 'backup in/out buffer equality', function() {
+    test( 'backup in/out buffer equality', function() {
       var gpt = GPT.parseBackup( backupData )
       var buffer = gpt.writeBackup()
       assert.equal( backupData.length, buffer.length )
       assert.deepEqual( backupData, buffer )
     })
 
-    it( 'verifies a backup GPT', function() {
+    test( 'verifies a backup GPT', function() {
       var gpt = GPT.parseBackup( backupData )
       assert.ok( gpt.verify() )
     })
 
-    it( 'writes a backup from primary', function() {
+    test( 'writes a backup from primary', function() {
       var gpt = GPT.parse( data, 512 )
       var buffer = gpt.writeBackupFromPrimary()
       assert.equal( backupData.length, buffer.length )
@@ -109,12 +109,12 @@ describe( 'GUID Partition Table', function() {
 
   })
 
-  describe( 'RASPBERRY PI MBR', function() {
+  context( 'RASPBERRY PI MBR', function() {
 
     const DATAPATH = __dirname + '/data/raspberry.bin'
     var data = fs.readFileSync( DATAPATH )
 
-    it( 'should throw "Invalid Signature"', function() {
+    test( 'should throw "Invalid Signature"', function() {
       assert.throws( function() {
         GPT.parse( data )
       })
